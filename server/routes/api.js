836273c6704273df5bea5d7c44516232dadb8d21
Abhinavsinghnegi <abhinavnegi842@gmail.com>
2022-12-29 11:43:19 +0530

    feat:create login API request

--- server/routes/api.js
+++ server/routes/api.js
@@ -29,7 +29,24 @@ router.post('/register',(req,res)=>{
     })
 })
 
+router.post('/login',(req,res)=>{
+    let userData = req.body
 
+    User.findOne({email:userData.email}, (error, user)=>{
+        if(error){
+            console.log(error);
+        } else{
+            if(!user){
+                res.status(401).send("Invalid user");
+            } else
+            if(user.password!==userData.password){
+                res.status(401).send('Invalid password');
+            } else{
+                res.status(200).send(user);
+            }
+        }
+    })
+})
 
 module.exports = router
 
