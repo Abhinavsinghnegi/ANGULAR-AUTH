1764c23e5455e9764c56efa93f041a2173ade509
Abhinavsinghnegi <abhinavnegi842@gmail.com>
2023-01-02 17:28:14 +0530

    feat:connect frontend with backend API

--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +10,16 @@ export class LoginComponent implements OnInit {
 
   loginUserData:any = {}
 
-  constructor() { }
+  constructor(private _auth :AuthService) { }
 
   ngOnInit(): void {
   }
 
   loginUser = () => {
-    console.log(this.loginUserData);
+    this._auth.loginUser(this.loginUserData).subscribe(
+      res => console.log(res),
+      err => console.log(err)
+    )
     
   }
 
